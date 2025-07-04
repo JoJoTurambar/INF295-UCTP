57ef9091f6d5fd10e3ca2171133dd2ab10a65f71
JoJoTurambar <jesus.escanilla@usm.com>
2025-07-04 08:26:08 -0400

    Final

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# INF295-UCTP
-Code of my UCTP problem for my artificial Intelligence class
+# Ejecución
+* Compilar y correr con make.
+* Limpiar con make clean
+
+# IA
+* Uso la extensión de Copilot en VS Code, lo que me ayuda a autocompletar el código.
+* Por falta de le pasé el código a Google AI Studio para detectar problemas de ineficiencia. Principalmente tuve problemas con checkeos extra en dos restricciones duras que hacían muy lenta la ejecución.
--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <numeric>
 
 using namespace std;
 
+// Global constants for reading instances and writing outputs
 const string instancesPath = "Inputs/";
 const string outputPath = "Outputs/";
 
@@ -12,223 +16,173 @@ const string filenameExtention = ".tim";
 const string filenameS = "small_";
 const string filenameM = "med_";
 const string filenameL = "big_";
-const vector<string> sizes = {"S"}; // {"S", "M", "L"}
+const vector<string> sizes = {"S", "M", "L"};
 
+// Periods and days
 const short P = 45; // Number of periods
 const short D = 5;  // Number of days
 const short P_per_D = 9; // Periods per day
 
+const int MAX_ITERATIONS = 100;
+// const int MAX_ITERATIONS_M = 100;
+// const int MAX_ITERATIONS_L = 10; 
+
 // weights
 const int w1 = 1;
 const int w2 = 1;
-const int w3 = 1;
+const int w3 = 4;
 
-// Hard constraints
-// -------------------------------------------------
+using Solution = vector<pair<int, int>>; // period - room
 
-// //TODO One event per room and period
-// bool EventRoomPeriod() {
-//     return true;
-// }
+int EvaluationFunction(const Solution& solution, int numStudents, int numEvents, const vector<int>& studentEvents);
 
-//TODO Student cannot have 2 events at the same time
-bool StudentEventConflict(int numStudents, int numEvents, vector<int> studentEvents, int event, const vector<pair<int, int>>& solution) {
-    // event: índice del evento a verificar
-    // solution: vector donde solution[e] = {periodo, sala}
-    int periodToCheck = solution[event].first;
-    if (periodToCheck == -1) return true; // No asignado aún
-
-    // Buscar el estudiante correspondiente a este evento
-    // studentEvents está en formato [s0e0, s0e1, ..., s1e0, s1e1, ...]
-    // Para cada estudiante s, revisa si tiene más de un evento en el mismo periodo
-    for (int s = 0; s < numStudents; ++s) {
-        // ¿El estudiante s asiste al evento 'event'?
-        if (studentEvents[s * numEvents + event] == 1) {
-            // Buscar todos los eventos de este estudiante
-            int count = 0;
-            for (int e = 0; e < solution.size(); ++e) {
-                if (studentEvents[s * numEvents + e] == 1 && solution[e].first == periodToCheck) {
-                    count++;
-                    if (count > 1) return false; // Más de un evento en el mismo periodo
-                }
-            }
-        }
+// Check hard constraints
+bool isFeasible(
+    int event, int period, int room,
+    const Solution& solution,
+    int numEvents, int numRooms, int numStudents, int numCharacteristics,
+    const vector<int>& rooms,
+    const vector<vector<bool>>& roomPeriodUsed,
+    const vector<int>& studentEvents,
+    const vector<int>& roomCharacteristics,
+    const vector<int>& eventCharacteristics)
+{
+    // One event per room and period
+    if (roomPeriodUsed[room][period]) {
+        return false;
     }
 
-    return true;
-}
-
-// Room must have the characteristics required by the event
-bool RoomCharacteristicsCheck(vector<int> roomCharacteristics, vector<int> eventCharacteristics, int event, int room, int numCharacteristics) {
-    for (int i = 0; i < numCharacteristics; i++) {
-        if (roomCharacteristics[numCharacteristics*room+i] != eventCharacteristics[numCharacteristics*event+i]){
+    // Room must have the characteristics required by the event
+    for (int c = 0; c < numCharacteristics; ++c) {
+        bool event_requires_feature = (eventCharacteristics[event * numCharacteristics + c] == 1);
+        bool room_has_feature = (roomCharacteristics[room * numCharacteristics + c] == 1);
+        if (event_requires_feature && !room_has_feature) {
             return false;
         }
     }
-    return true;
-}
 
-// Amount of students must not exceed the room capacity
-bool RoomCapacity(int numStudents, int numEvents, vector<int> studentEvents, vector<int> rooms, int room, int event) {
+    // Amount of students must not exceed the room capacity
     int studentsAttendingEvent = 0;
-    int stuEventsSize = studentEvents.size();
-    for (int i = event; i < stuEventsSize; i = i + numEvents) { // check 1 event for each student
-        if (studentEvents[i] == 1) {
+    for (int s = 0; s < numStudents; ++s) {
+        if (studentEvents[s * numEvents + event] == 1) {
             studentsAttendingEvent++;
         }
     }
-    // cout << "room " <<   studentsAttendingEvent << endl;
-    printf("room %d, capacity %d, students attending %d\n", room, rooms[room], studentsAttendingEvent);
-    if (studentsAttendingEvent <= rooms[room]) {
-        return true;
+    if (studentsAttendingEvent > rooms[room]) {
+        return false;
     }
-    return false;
-}
 
-bool HardConstraints(
-    vector<pair<int, int>> &solution, int numEvents, int numRooms, int numStudents, 
-    vector<int> &studentEvents, vector<int> &rooms, int numCharacteristics, 
-    vector<int> &roomCharacteristics, vector<int> &eventCharacteristics, int e, int r
-) {
-    // Check one event per room and period
-    // if (!EventRoomPeriod()) {
-    //     return false;
-    // }
-
-    // Check student event conflicts
+    // A student cannot have two events at the same time
     for (int s = 0; s < numStudents; ++s) {
-        if (!StudentEventConflict(numStudents, numEvents, studentEvents, s, solution)) {
-            return false;
-        }
-    }
+        if (studentEvents[s * numEvents + event] == 1) { // If s is attendint the event
+            for (int other_e = 0; other_e < numEvents; ++other_e) { // Check other events
+                if (event == other_e) continue; // Don't compare event to itself
 
-    // Check room characteristics and capacity for the given event and room
-    if (!RoomCharacteristicsCheck(roomCharacteristics, eventCharacteristics, e, r, numCharacteristics)) {
-        return false;
-    }
-    if (!RoomCapacity(numStudents, numEvents, studentEvents, rooms, r, e)) {
-        return false;
+                if (studentEvents[s * numEvents + other_e] == 1 && solution[other_e].first == period) {
+                    return false;
+                }
+            }
+        }
     }
 
     return true;
 }
 
-// -------------------------------------------------
-
-// Soft constraints. Return 1 if the student meets the condition
-// -------------------------------------------------
-
-// Studen must not have an unique event in one day
-// int StudentSingleEvent(vector<pair<int, int>> &solution, int numStudents, vector<int> &studentEvents,  int numEvents) {
-//     if 
-// }
-
-// Student must not have 2 events at the end of the day
-// int StudentEventsEndDay() {
-//     cout << "not implemented yet" << endl;
-//     return 0;
-// }
-
-// // Student must not have more than 2 consecutive events
-// int Student2ConsecutiveEvents() {
-//     cout << "not implemented yet" << endl;
-//     return 0;
-// }
-
 // Evaluation Function
 int EvaluationFunction(
-    vector<pair<int, int>> &solution, int numStudents, vector<int> &studentEvents,  int numEvents
+    const Solution& solution, int numStudents, int numEvents, const vector<int>& studentEvents
 ) {
-    int sge = 0;
-    int seed = 0;
-    int s2ce = 0;
-    int periodEvent, day;
-    // SigleEvent
+    int result = 0;
+
     for (int s = 0; s < numStudents; s++) { // for each student
-        vector<int> studentEventsWeek;
-        studentEventsWeek.assign(P, 0);
+        vector<int> studentSchedule;
 
-        day = 0; // reset day
-        int numEvents1 = 0;
-        int numEvents2 = 0;
-
-        // Mark occupied slots in the week of the current stundent
-        for (int sEvents = s; sEvents < numEvents; sEvents++) {
-            if (studentEvents[sEvents] == 1) {
-                periodEvent = solution[sEvents].first; 
-                studentEventsWeek[periodEvent] = 1;
-                numEvents1++;
+        for (int e = 0; e < numEvents; ++e) { // for each event
+            // Check if the event is assigned and if the student attends it
+            if (solution[e].first != -1 && studentEvents[s * numEvents + e] == 1) {
+                studentSchedule.push_back(solution[e].first);
             }
         }
+        sort(studentSchedule.begin(), studentSchedule.end()); // sort periods
 
-        for (auto event : studentEventsWeek) {
-            if (event == 1) {
-                numEvents2++;
-            }
+        int studentScheduleSize = studentSchedule.size();
+
+        // if the student has no events, continue to the next student
+        if (studentSchedule.empty()) {
+            continue;
         }
 
-        cout << "studentEvents" << numEvents1 << endl;
-        cout << "studentEventsWeek" << numEvents2 << endl;
+        // Penalties
+        int singleEventDays = 0;
+        int threeConsecutive = 0;
+        int lastPeriodEvents = 0;
+        for (int d = 0; d < D; ++d) { // D = 5 days
+            vector<int> periodsOnDay;
+            int eventsOnDay = 0;
+            int dayStartPeriod = d * P_per_D; // if d = 2: dayStartPeriod = 2 * 9 = 18
+            int dayEndPeriod = dayStartPeriod + P_per_D; // P_per_D = 9
 
-        // SingleEvent
-        int count = 0; // Forma actual contar sólo 1 SingleEvent por estudiante
-        for (day = 0; day < 5; day++) { // for each day of the week
-            for (int i = day*9; i < (day+1)*9 - 1; i++) { // check periods in the day
-                if (studentEventsWeek[i] == 1) {
-                    count++;
+            for (int period : studentSchedule) {
+                if (period >= dayStartPeriod && period < dayEndPeriod) {
+                    eventsOnDay++;
+                    periodsOnDay.push_back(period - dayStartPeriod); // 0,1,...,8
                 }
             }
-            if (count == 1) {
-                sge++;
-                break;
+
+            sort(periodsOnDay.begin(), periodsOnDay.end());
+
+            // Add Singe Event
+            if (eventsOnDay == 1) {
+                singleEventDays++;
             }
 
-            // EndDay
-            if (studentEventsWeek[(day+1)*9-1] == 1) {
-                seed++;
-            }
-
-            // 2Consecutive
-            for (int i = day; i < day*9 - 2; i++) {
-                if (studentEventsWeek[i] == 1 && studentEventsWeek[i+1] == 1 && studentEventsWeek[i+2] == 1) {
-                    s2ce++;
+            // Add 3 consecutive event
+            
+            for (int i = 0; i + 2 < studentScheduleSize; ++i) {
+                if (studentSchedule[i+1] == studentSchedule[i] + 1 && studentSchedule[i+2] == studentSchedule[i] + 2) {
+                    threeConsecutive++;
                 }
             }
-            cout << "day: " << day << endl;
-            count = 0; // reset count for the next day
+
+            // Add EndDay
+            if (!periodsOnDay.empty() && (periodsOnDay.back() + 1) % P_per_D == 0) { // last period of the day
+                lastPeriodEvents++;
+            }
         }
-        if (seed != 0) {
-            seed = 1;
-        }
-        if (s2ce != 0) {
-            s2ce = 1;
-        }
-        cout << "student. " << s << endl;
+
+        result += w1 * singleEventDays + w2 * lastPeriodEvents + w3 * threeConsecutive;
     }
-    // }
 
-    int result = w1 * sge + w2 * seed + w3 * s2ce;
     return result;
 }
 
+
+
 void Greedy(
-    vector<pair<int, int>>& solution, int numEvents, int numRooms, int numStudents, vector<int> &studentEvents, 
-    vector<int> &rooms, int numCharacteristics, vector<int> &roomCharacteristics, vector<int> &eventCharacteristics
+    Solution& solution, int numEvents, int numRooms, int numStudents, vector<int> &studentEvents, 
+    vector<int> &rooms, int numCharacteristics, vector<int> &roomCharacteristics, vector<int> &eventCharacteristics,
+    bool randomize
 ) {
     // Inicializa la solución vacía
     solution.assign(numEvents, {-1, -1});
     vector<vector<bool>> roomPeriodUsed(numRooms, vector<bool>(P, false));
 
-    for (int e = 0; e < numEvents; ++e) { // For each event
+    vector<int> eventOrder(numEvents);
+    iota(eventOrder.begin(), eventOrder.end(), 0); // Fills with 0, 1, 2,...
+    // Randomize for HCFI
+    if (randomize) {
+        random_device rd;
+        mt19937 g(rd());
+        shuffle(eventOrder.begin(), eventOrder.end(), g);
+    }
+
+    for (int e : eventOrder) { // For each event
         bool assigned = false; // the event has not been assigned yet
         for (int p = 0; p < P && !assigned; ++p) { // For each period
             for (int r = 0; r < numRooms && !assigned; ++r) { // iterate over rooms and check if the event has not been assigned yet
                 // Check hard constraints
-                if (
-                    HardConstraints(solution, numEvents, numRooms, numStudents, studentEvents, rooms, numCharacteristics, 
-                        roomCharacteristics, eventCharacteristics, e, r) &&
-                        !roomPeriodUsed[r][p] // Check if the room and period are not already used
-                    ) 
+                if (isFeasible(e, p, r, solution, numEvents, numRooms, numStudents, numCharacteristics,
+                               rooms, roomPeriodUsed, studentEvents, roomCharacteristics, eventCharacteristics))
                     {
                     solution[e] = {p, r}; // period - room
                     roomPeriodUsed[r][p] = true;
@@ -236,17 +190,47 @@ void Greedy(
                 }
             }
         }
-        if (!assigned) {
-            // cerr << "Wasn't possible to assign event " << e << endl;
-        }
+        // if (!assigned) {
+        //     cerr << "Wasn't possible to assign event " << e << endl;
+        // }
     }
 }
 
-//TODO Hill Climbing First Improvement
-void HCFI() {
-    // Implement the Hill Climbing First Improvement algorithm here
-    cout << "Hill Climbing First Improvement not implemented yet." << endl;
-    // Implementar movimiento de aleatoriedad
+void HCFI(
+    Solution& solution,
+    int numEvents, int numRooms, int numStudents, int numCharacteristics,
+    vector<int>& rooms,
+    vector<int>& studentEvents,
+    vector<int>& roomCharacteristics,
+    vector<int>& eventCharacteristics)
+{
+    cout << endl << "Starting Hill Climbing...\n" << endl;
+    int currentCost = EvaluationFunction(solution, numStudents, numEvents, studentEvents);
+    cout << "Initial Cost: " << currentCost << endl;
+    int iterations = 0;
+
+    bool improvementFound;
+    do {
+        improvementFound = false;
+        Solution newSolution;
+        Greedy(newSolution, numEvents, numRooms, numStudents, studentEvents, 
+            rooms, numCharacteristics, roomCharacteristics, eventCharacteristics, true);
+        int newCost = EvaluationFunction(newSolution, numStudents, numEvents, studentEvents);
+        if (newCost < currentCost) {
+            // If the new solution is better, update the current solution
+            solution = newSolution;
+            currentCost = newCost;
+            improvementFound = true;
+            iterations++;
+            cout << "Improvement found! New cost: " << currentCost << " after " << iterations << " iterations." << endl;
+        } else {
+            cout << "No improvement found. Current cost: " << currentCost << endl;
+            
+        }
+
+    } while (improvementFound && iterations < MAX_ITERATIONS);
+
+    cout << "Hill Climbing finished. Final cost: " << currentCost << endl;
 }
 
 string GetFileName(int i, string size) {
@@ -261,7 +245,7 @@ string GetFileName(int i, string size) {
     }
 }
 
-void WriteSolution(vector<pair<int, int>> &solution, string &filename) {
+void WriteSolution(const Solution &solution, string &filename) {
     ofstream file(outputPath+filename);
     if (!file.is_open()) {
         cerr << "Error opening output file: " << filename << endl;
@@ -320,18 +304,17 @@ void SolveInstance(string& fileName) {
     file.close();
 
     // Get initial solution
-    vector<pair<int, int>> solution; // periodo sala
+    Solution solution; // periodo sala
     Greedy(solution, numEvents, numRooms, numStudents, studentEvents, rooms, numCharacteristics, roomCharacteristics,
-    eventCharacteristics);
+    eventCharacteristics, false);
+
+    // int initialCost = EvaluationFunction(solution, numStudents, numEvents, studentEvents);
+    printf("\nFile : %s\n", fileName.c_str());
 
     // Apply Hill Climbing First Improvement
-    // HCFI();
+    HCFI(solution, numEvents, numRooms, numStudents, numCharacteristics,rooms, studentEvents, roomCharacteristics, eventCharacteristics);
 
     WriteSolution(solution, fileName);
-
-    // cout << EvaluationFunction(solution, numStudents, studentEvents, numEvents) << endl;
-    // printf("%s: %d\n", fileName.c_str(), EvaluationFunction(solution, numStudents, studentEvents, numEvents));
-    
 }
 
 int main () {
@@ -340,8 +323,8 @@ int main () {
 
     //
     // Read all instances
-    for (int j = 0; j < 1; j++){
-        for (int i = 1; i <= 1; i++) {
+    for (int j = 0; j < 3; j++){
+        for (int i = 1; i <= 20; i++) {
             filename = GetFileName(i, sizes[j]);
             // filename = "small_1.tim";
             SolveInstance(filename);
